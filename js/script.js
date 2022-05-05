5b362e8e966aebbc98f2b9e878fcbbe845a363cb
Evgen Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-05-05 16:36:31 +0000

    fix comment format

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 // This file contains the JS functions for index.html
 
 'use strict'
-/*** This function calculates the volume of a pyramid.***/
+/**
+* This function calculates the volume of a pyramid.
+*/
 function calculate() {
   
   // input
